3dc205a8ad990150c69a42c8dd8a945151c585ea
Jacopo Marrone <jacopo.marrone27@gmail.com>
2023-01-27 23:22:32 +0100

    feat: add new demos

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,6 @@ import { Demo002 } from './demos/demo-002';
 import { Demo003 } from './demos/demo-003';
 import { Demo004 } from './demos/demo-004';
 import { Demo005 } from './demos/demo-005';
+import { Demo006 } from './demos/demo-006';
 
-export const App = () => <Demo005 />;
+export const App = () => <Demo006 />;
--- src/demos/demo-006.tsx
+++ src/demos/demo-006.tsx
@@ -5,10 +5,13 @@ import { createEveryNTimer, initializeWorld } from '../modules/three-helpers';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
 import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass';
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { FilmShader } from 'three/examples/jsm/shaders/FilmShader';
+import { StaticShader } from '../modules/three-helpers/shaders/StaticShader';
 
 
 // Main React Component
-export const Demo004 = () => {
+export const Demo006 = () => {
   const canvasNodeRef = useRef<HTMLCanvasElement>(null);
   const [errorMessage, setErrorMessage] = useState('');
   const [debugJSON, setDebugJSON] = useState({});
@@ -28,9 +31,24 @@ export const Demo004 = () => {
   return (
     <>
       <header>
-        <h1>Demo004</h1>
+        <h1>Demo006</h1>
       </header>
-      <canvas ref={canvasNodeRef} id="three-js-canvas" />
+      <main style={{ position: 'relative' }}>
+        <img
+          src="https://images.unsplash.com/photo-1674590462122-ae799c236b08?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1065&amp;q=80"
+          style={{
+            zIndex: 0,
+            width: "61%",
+            position: "absolute",
+            left: "0",
+          }}
+        />
+        <canvas ref={canvasNodeRef} id="three-js-canvas" style={{
+          zIndex: 1,
+          position: 'relative',
+          mixBlendMode: 'lighten',
+        }} />
+      </main>
       <footer>
         {errorMessage && <p>{errorMessage}</p>}
         {debugJSON && <pre>{JSON.stringify(debugJSON, null, 2)}</pre>}
@@ -44,16 +62,17 @@ const main = async (canvas: HTMLCanvasElement, setDebugJSON: (json: object) => v
   // ===============================================
   //     Global Settings
   // ===============================================
-  const { scene, camera, gui, grid, composer, renderComposer } = initializeWorld(canvas);
+  const { scene, camera, gui, grid, clock, axesHelper, composer, renderComposer } = initializeWorld(canvas);
   camera.position.z = 80;
   camera.position.x = 1;
   camera.position.y = 0;
   camera.rotation.x = -26;
   camera.rotation.y = -2;
-  camera.fov = 70;
+  camera.fov = 4;
   camera.updateProjectionMatrix();
-  // scene.background = new THREE.Color(0xf00);
+  scene.background = new THREE.Color(0x333);
   grid.visible = false;
+  axesHelper.visible = false;
 
   // ===============================================
   //     Lights
@@ -70,27 +89,27 @@ const main = async (canvas: HTMLCanvasElement, setDebugJSON: (json: object) => v
   guiAL.addColor({ color: ambientLight.color.getHex() }, 'color').onChange(color => ambientLight.color.set(color));
   // guiAL.open();
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.visible = false;
-  // directionalLight.castShadow = true;
-  directionalLight.position.set(0, 32, 64);
-  lightsGroup.add(directionalLight);
-  const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
-  lightsGroup.add(directionalLightHelper);
-  const guiDL = gui.addFolder('Directional Light');
-  guiDL.add(directionalLight, 'visible');
-  guiDL.add(directionalLight, 'castShadow');
-  guiDL.add(directionalLight, 'intensity', 0, 4, 0.01);
-  guiDL.addColor({ color: directionalLight.color.getHex() }, 'color').onChange(color => directionalLight.color.set(color));
-  const guiDLPosition = guiDL.addFolder('Position');
-  guiDLPosition.add(directionalLight.position, 'z', -4, 4, 0.01);
-  guiDLPosition.add(directionalLight.position, 'x', -4, 4, 0.01);
-  guiDLPosition.add(directionalLight.position, 'y', -4, 10, 0.01);
-  const guiDLRotation = guiDL.addFolder('Rotation');
-  guiDLRotation.add(directionalLight.rotation, 'z', -4, 4, 0.01);
-  guiDLRotation.add(directionalLight.rotation, 'x', -4, 4, 0.01);
-  guiDLRotation.add(directionalLight.rotation, 'y', -4, 4, 0.01);
-  // guiDL.open();
+  // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  // directionalLight.visible = false;
+  // // directionalLight.castShadow = true;
+  // directionalLight.position.set(0, 32, 64);
+  // lightsGroup.add(directionalLight);
+  // const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+  // lightsGroup.add(directionalLightHelper);
+  // const guiDL = gui.addFolder('Directional Light');
+  // guiDL.add(directionalLight, 'visible');
+  // guiDL.add(directionalLight, 'castShadow');
+  // guiDL.add(directionalLight, 'intensity', 0, 4, 0.01);
+  // guiDL.addColor({ color: directionalLight.color.getHex() }, 'color').onChange(color => directionalLight.color.set(color));
+  // const guiDLPosition = guiDL.addFolder('Position');
+  // guiDLPosition.add(directionalLight.position, 'z', -4, 4, 0.01);
+  // guiDLPosition.add(directionalLight.position, 'x', -4, 4, 0.01);
+  // guiDLPosition.add(directionalLight.position, 'y', -4, 10, 0.01);
+  // const guiDLRotation = guiDL.addFolder('Rotation');
+  // guiDLRotation.add(directionalLight.rotation, 'z', -4, 4, 0.01);
+  // guiDLRotation.add(directionalLight.rotation, 'x', -4, 4, 0.01);
+  // guiDLRotation.add(directionalLight.rotation, 'y', -4, 4, 0.01);
+  // // guiDL.open();
 
   // const spotLight = new THREE.SpotLight(0xffdd00, 0.5);
   // spotLight.visible = true;
@@ -148,58 +167,100 @@ const main = async (canvas: HTMLCanvasElement, setDebugJSON: (json: object) => v
   // ===============================================
 
   //Object - Ground;
-  const groundMesh = new THREE.Mesh(
-    new THREE.BoxGeometry(8, 0.5, 8),
-    new THREE.MeshPhongMaterial({ color: 0xfffffa }),
-  );
-  groundMesh.receiveShadow = true;
-  groundMesh.position.y = groundMesh.geometry.parameters.height * 0.5 * -1;
-  scene.add(groundMesh);
-
-  // Object - Origin Cube
-  // const cuboOrigin = new THREE.Mesh(
-  //   new THREE.BoxGeometry(0.1, 0.1, 0.1),
-  //   new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+  // const groundMesh = new THREE.Mesh(
+  //   new THREE.BoxGeometry(8, 0.5, 8),
+  //   new THREE.MeshPhongMaterial({ color: 0xfffffa }),
   // );
-  // scene.add(cuboOrigin);
+  // groundMesh.position.y = groundMesh.geometry.parameters.height * 0.5 * -1;
+  // groundMesh.rotation.x = Math.PI / 4;
+  // scene.add(groundMesh);
 
-  //POST PROCESSING
+
+
+  // ===============================================
+  //     POST-PROCESSSING
+  // ===============================================
   const renderPass = new RenderPass(scene, camera);
-  composer.addPass(renderPass);
+
+
+  const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
+  const bloomParams = {
+    bloomThreshold: 0,
+    bloomStrength: .9,
+    bloomRadius: 0.7,
+  };
+  const guiBloom = gui.addFolder('Bloom');
+  guiBloom.add(bloomParams, 'bloomThreshold', 0.0, 1.0).onChange(onParamsChange);
+  guiBloom.add(bloomParams, 'bloomStrength', 0.0, 3.0).onChange(onParamsChange);
+  guiBloom.add(bloomParams, 'bloomRadius', 0.0, 1.0).step(0.01).onChange(onParamsChange);
+  // guiBloom.open();
+
+
+  const filmPass = new ShaderPass(FilmShader);
+  // filmPass.uniforms.grayscale.value = 0;
+  const filmParams = {
+    show: true,
+    count: 800,
+    sIntensity: 0.9,
+    nIntensity: 0.4
+  };
+  var f3 = gui.addFolder('Scanlines');
+  f3.add(filmParams, 'count', 50, 1000).onChange(onParamsChange);
+  f3.add(filmParams, 'sIntensity', 0.0, 2.0).step(0.1).onChange(onParamsChange);
+  f3.add(filmParams, 'nIntensity', 0.0, 2.0).step(0.1).onChange(onParamsChange);
+  f3.open();
+
+
+  const staticPass = new ShaderPass(StaticShader);
+  const staticParams = {
+    show: true,
+    amount: 0.3,
+    size: 1.0
+  };
+  var f4 = gui.addFolder('Static');
+  f4.add(staticParams, 'amount', 0.0, 1.0).step(0.01).listen().onChange(onParamsChange);
+  f4.add(staticParams, 'size', 1.0, 100.0).step(1.0).onChange(onParamsChange);
+  f4.open();
+
 
   const glitchPass = new GlitchPass();
-  composer.addPass(glitchPass);
-
-  const bloomPass = new UnrealBloomPass(
-    new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85
-  );
-  const params = {
-    bloomStrength: 2,
-    bloomThreshold: 0,
-    bloomRadius: 0.1,
-  };
-  bloomPass.threshold = params.bloomThreshold;
-  bloomPass.strength = params.bloomStrength;
-  bloomPass.radius = params.bloomRadius;
-  composer.addPass(bloomPass);
-  const guiBloom = gui.addFolder('Bloom');
-  guiBloom.add(params, 'bloomThreshold', 0.0, 1.0).onChange(value => { bloomPass.threshold = Number(value); });
-  guiBloom.add(params, 'bloomStrength', 0.0, 3.0).onChange(value => { bloomPass.strength = Number(value); });
-  guiBloom.add(params, 'bloomRadius', 0.0, 1.0).step(0.01).onChange(value => { bloomPass.radius = Number(value); });
 
 
+  // mount render passes
+  composer.addPass(renderPass);
+  composer.addPass(filmPass);
+  composer.addPass(staticPass);
+  // composer.addPass(glitchPass);
+  staticPass.renderToScreen = true;
+
+
+  function onParamsChange() {
+    bloomPass.threshold = bloomParams.bloomThreshold;
+    bloomPass.strength = bloomParams.bloomStrength;
+    bloomPass.radius = bloomParams.bloomRadius;
+
+    filmPass.uniforms['sCount'].value = filmParams.count;
+    filmPass.uniforms['sIntensity'].value = filmParams.sIntensity;
+    filmPass.uniforms['nIntensity'].value = filmParams.nIntensity;
+
+    staticPass.uniforms['amount'].value = staticParams.amount;
+    staticPass.uniforms['size'].value = staticParams.size;
+  }
+  onParamsChange();
 
   // ===============================================
   //     Render
   // ===============================================
 
   const animate = () => {
-    //   // Debug in React View
-    //   setDebugJSON({});
-    //
-    groundMesh.rotation.y += 0.01;
+    const shaderTime = clock.getElapsedTime();
+    filmPass.uniforms['time'].value = shaderTime;
+    staticPass.uniforms['time'].value = shaderTime;
+
     window.requestAnimationFrame(animate);
     renderComposer();
+    // Debug in React View
+    // setDebugJSON();
   };
   animate();
 
